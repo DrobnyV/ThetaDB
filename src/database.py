e185d0e59060379ac6db69a77ffca7a64f49ab34
Vojtus <diminutive.diminutive@gmail.com>
2025-01-15 08:58:04 +0100

    Commit

--- src/database.py
+++ src/database.py
@@ -1,42 +1,19 @@
-# /src/database.py
+from configparser import ConfigParser
 
-import mysql.connector
-from mysql.connector import Error
-import configparser
+import mysql
 
 
-class DatabaseConnection:
-    def __init__(self, config_file='../config.ini'):
-        self.config = configparser.ConfigParser()
-        self.config.read(config_file)
-        self.host = self.config['database']['host']
-        self.user = self.config['database']['user']
-        self.password = self.config['database']['password']
-        self.database = self.config['database']['database']
-        self.connection = None
+def get_db_connection():
+    config = ConfigParser()
+    config.read('config.ini')
 
-    def connect(self):
-        try:
-            self.connection = mysql.connector.connect(
-                host=self.host,
-                user=self.user,
-                password=self.password,
-                database=self.database
-            )
-            if self.connection.is_connected():
-                print("Úspěšně připojeno k MySQL databázi")
-        except Error as e:
-            print(f"Chyba při připojování k MySQL databázi: {e}")
-
-    def disconnect(self):
-        if self.connection.is_connected():
-            self.connection.close()
-            print("Spojení s databází bylo ukončeno")
-
-    def get_connection(self):
-        return self.connection
-
-# Příklady použítí:
-connection = DatabaseConnection()
-connection.connect()
-connection.disconnect()
+    try:
+        return mysql.connector.connect(
+            host=config['database']['host'],
+            user=config['database']['user'],
+            password=config['database']['password'],
+            database=config['database']['database']
+        )
+    except mysql.connector.Error as err:
+        print(f"Error connecting to the database: {err}")
+        return None
